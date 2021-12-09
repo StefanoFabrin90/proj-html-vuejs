66636bcdf68e521c0e0e600b4762bb4f90be3c50
stef_fab@hotmail.it <stef_fab@hotmail.it>
2021-12-09 11:00:09 +0100

    add 1 collegamento 9/12/2021

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- HEADER -->
+    <header>
+      <!-- section-1 header -->
+      <Sec1Header />
+
+    </header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sec1Header from '@/components/Sec1Header.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Sec1Header,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Sec1Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+      <h1>test</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Sec1Header'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
